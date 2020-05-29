22997e5748b1f72fc0e6c943665cd4b15d47fbfd
rekuch-nick <45768867+rekuch-nick@users.noreply.github.com>
2020-05-28 19:55:59 -0500

    reads from files now

--- src/squadMay21/Main.java
+++ src/squadMay21/Main.java
@@ -1,5 +1,10 @@
 package squadMay21;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import squadMay21.stats.IQRange;
 import squadMay21.stats.LinearReg;
 import squadMay21.stats.Mean;
@@ -13,15 +18,50 @@ import squadMay21.stats.Variance;
 
 public class Main {
 
+	static List<Integer> xData;
+	static List<Integer> yData;
+	
 	public static void main(String[] args) {
 		
+		xData = new ArrayList<>();
+		yData = new ArrayList<>();
+		
+		//readFromFile();
+		//int[] xArr = xData.stream().mapToInt(Integer::intValue).toArray();
+		//int[] yArr = yData.stream().mapToInt(Integer::intValue).toArray();
 		int[] xArr = {1, 2, 3, 4, 5, 6, 7, 8};
-		int[] yArr = {2, 6, 9, 10, 12, 16, 17, 21};
+		int[] yArr = {1, 2, 3, 4, 5, 6, 7, 8};
+		//int[] yArr = {2, 4, 6, 8, 10, 12, 14, 16};		
 		
 		System.out.print("X : ");
 		printArray(xArr);
 		System.out.print("Y : ");
 		printArray(yArr);
+		
+		System.out.println();
+		System.out.println("X Stats:");
+		System.out.println("Mean: " + Mean.getMean(xArr));
+		System.out.println("Median: " + Median.getMedian(xArr));
+		System.out.print("Mode: ");
+		Mode.printMode(xArr);
+		System.out.println("Range: " + Range.getRange(xArr));
+		System.out.println("IQ Range: " + IQRange.getIQRange(xArr));
+		System.out.println("Variance: " + Variance.getVariance(xArr));
+		System.out.println("Standard Dev: " + StandardDeviation.getStandardDeviation(xArr));
+		
+		System.out.println();
+		System.out.println("Y Stats:");
+		System.out.println("Mean: " + Mean.getMean(yArr));
+		System.out.println("Median: " + Median.getMedian(yArr));
+		System.out.print("Mode: ");
+		Mode.printMode(yArr);
+		System.out.println("Range: " + Range.getRange(yArr));
+		System.out.println("IQ Range: " + IQRange.getIQRange(yArr));
+		System.out.println("Variance: " + Variance.getVariance(yArr));
+		System.out.println("Standard Dev: " + StandardDeviation.getStandardDeviation(yArr));
+		
+		System.out.println();
+		
 		
 		System.out.print("Pearson: ");
 		System.out.println(PearsonCoefficient.getPearsonCoefficient(xArr, yArr));
@@ -32,28 +72,33 @@ public class Main {
 		System.out.print("Y-Intercept: ");
 		System.out.println(LinearReg.getIntercept(xArr, yArr));
 		
-		/*
-		System.out.print("Data: ");
-		printArray(arr);
-		Sort.sortArray(arr);
-		System.out.print("Sorted: ");
-		printArray(arr);
-		
-		System.out.println("Mean: " + Mean.getMean(arr));
-		System.out.println("Median: " + Median.getMedian(arr));
-		System.out.print("Mode: ");
-		Mode.printMode(arr);
-		System.out.println("Range: " + Range.getRange(arr));
-		System.out.println("IQ Range: " + IQRange.getIQRange(arr));
-		System.out.println("Variance: " + Variance.getVariance(arr));
-		System.out.println("Standard Dev: " + StandardDeviation.getStandardDeviation(arr));
-		*/
-		
-
 	}
 	
-	
-	
+	private static void readFromFile() {
+		String fileName = "stockdata.csv";
+		try {
+			FileReader reader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(reader);
+			
+			
+			
+			String line = "";
+			while ((line = bufferedReader.readLine()) != null) {
+				
+				String[] thisLine = line.split(",");
+				
+				int yy = (int)Float.parseFloat(thisLine[0]);
+				int xx = (int)Float.parseFloat(thisLine[1]);
+				
+				xData.add( xx );
+				yData.add( yy );
+			}
+			
+			bufferedReader.close();
+		} catch (Exception e) {
+			System.out.println("I/O Error");
+		}
+	}
 	
 	private static void printArray(int[] arr) {
 		for(int i : arr) {
